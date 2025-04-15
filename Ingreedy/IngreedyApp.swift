e3ab2fad56849c1b856d990da8521471e8ee8ada
Mert Yılmazer <mertyilmazer25@gmail.com>
2025-04-15 17:36:04 +0300

    Initial Commit

---
+++ Ingreedy/IngreedyApp.swift
@@ -0,0 +1,17 @@
+//
+//  IngreedyApp.swift
+//  Ingreedy
+//
+//  Created by Mert Yılmazer on 15.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct IngreedyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
